210a1639fcc3b0133c5adc4c70301ff93310c37e
Igor <igor@nemkovich.dev>
2024-12-30 00:08:42 +0100

    3. Moving Around

--- main.py
+++ main.py
@@ -3,20 +3,28 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    # pygame.display.set_caption("Asteroids")
-    pygame.Surface.fill(screen, (0, 0, 0))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
-    pygame.display.flip()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
+
+        player.draw(screen)
+        player.update(dt)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
